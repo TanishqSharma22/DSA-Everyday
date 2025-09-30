031d4ad8b8d34064e23f291853bd90a1a1560c7b
TanishqSharma22 <tanishq8650@gmail.com>
2025-09-30 17:29:30 +0530

    Add RomanToIntegers.java to STRINGS folder

---
+++ STRINGS/RomanToIntegers.java
@@ -0,0 +1,37 @@
+package STRINGS;
+
+public class RomanToIntegers {
+    public int getValue(char ch) {
+        switch (ch) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+    public int romanToInt(String s) {
+        int n = s.length();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            char ch1 = s.charAt(i);
+            if( (i+1) < n && getValue(ch1) < getValue(s.charAt(i+1)) ){
+                sum = sum - getValue(ch1);
+            }else{
+                sum = sum + getValue(ch1);
+            }
+            }
+            return sum;
+        }
+    
+    public static void main(String args[]){
+        RomanToIntegers rti = new RomanToIntegers();
+        String s = "MCMXCIV";
+        System.out.println(rti.romanToInt(s));
+        
+    }
+    
+}
