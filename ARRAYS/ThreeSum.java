293e620d7c3e4531d32b86345e6767d97d24c0f2
TanishqSharma22 <tanishq8650@gmail.com>
2026-06-20 02:03:00 +0530

    Add ThreeSum.java to ARRAYS folder

---
+++ ARRAYS/ThreeSum.java
@@ -0,0 +1,46 @@
+/*input: nums = [2, -2, 0, 3, -3, 5]
+Output: [[-2, 0, 2], [-3, -2, 5], [-3, 0, 3]]
+Explanation:
+nums[1] + nums[2] + nums[0] = 0
+nums[4] + nums[1] + nums[5] = 0
+nums[4] + nums[2] + nums[3] = 0 */
+package ARRAYS;
+import java.util.*;
+public class ThreeSum {
+    public List<List<Integer>> sumThree(int[] nums) {
+    Arrays.sort(nums);
+    List<List<Integer>> result = new ArrayList<>();
+
+    for (int i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        int left = i + 1;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum == 0) {
+                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                left++;
+                right--;
+
+                while (left < right && nums[left] == nums[left - 1]) left++;
+                while (left < right && nums[right] == nums[right + 1]) right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
+public static void main(String[] args) {
+    ThreeSum ts = new ThreeSum();
+    int[] nums = {2, -2, 0, 3, -3, 5};
+    List<List<Integer>> result = ts.sumThree(nums);
+    System.out.println(result);
+}
+}
